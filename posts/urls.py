1b9eb6cf8027abe2f1d11f42073346cfb508b066
Mark Zsombor <markszsombor@gmail.com>
2018-07-05 20:13:51 -0700

    added the ability to update posts

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     re_path(r'^$', post_list, name='post_list'),
     re_path(r'^create/$', post_create, name='post_create'),
     re_path(r'^(?P<id>\d+)/$', post_detail, name='detail'),
-    re_path(r'^update/$', post_update, name='post_update'),
+    re_path(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
     re_path(r'^delete/$', post_delete, name='post_delete'),
 ]
