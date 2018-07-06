ac52a16c1e43fa2e83e19e9f2ffca95478c9effc
Mark Zsombor <markszsombor@gmail.com>
2018-07-05 20:40:14 -0700

    added route for deleting posts

--- posts/urls.py
+++ posts/urls.py
@@ -3,9 +3,9 @@ from posts.views import *
 
 #  re_path allows for regex to define routes
 urlpatterns = [
-    re_path(r'^$', post_list, name='post_list'),
-    re_path(r'^create/$', post_create, name='post_create'),
+    re_path(r'^$', post_list, name='list'),
+    re_path(r'^create/$', post_create, name='create'),
     re_path(r'^(?P<id>\d+)/$', post_detail, name='detail'),
     re_path(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
-    re_path(r'^delete/$', post_delete, name='post_delete'),
+    re_path(r'^(?P<id>\d+)/delete/$', post_delete, name='delete'),
 ]
